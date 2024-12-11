5b839fbfa5f814ff28251aca530cf5d696285cbf
Mich <paugam21@outlook.com>
2024-12-10 20:56:22 -0600

    alertas y registro de usuarios

--- js/logout.js
+++ js/logout.js
@@ -3,9 +3,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const logoutButton = document.getElementById('logoutButton');
 
     if (logoutButton) {
-        logoutButton.addEventListener('click', () => {
+        logoutButton.addEventListener('click', async () => {
             localStorage.removeItem('usuario'); // Eliminar información del usuario
-            alert('Sesión cerrada');
+            await Swal.fire({
+                title: "Salió de su cuenta",
+                text: "Ha salido correctamente su cuenta",
+                color: "#000000",
+                background: "#ffffff",
+                customClass: {
+                    confirmButton: 'btn-succ',
+                    title: 'title-class', 
+                    content: 'content-class' 
+                },
+                buttonsStyling: false,
+            });
             window.location.reload();
         });
     }
