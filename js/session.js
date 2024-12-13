814c44280324d4cff2d03cdf80ec3965ba747a6f
NadiaAcostaM <acostamirandanadiaa@gmail.com>
2024-12-12 21:20:35 -0600

    Mostrar preferencias

--- js/session.js
+++ js/session.js
@@ -1,15 +1,20 @@
-// Obtener usuario (LocalStorage)
+// Obtener el usuario y las categorías desde localStorage
 let usuario = JSON.parse(localStorage.getItem('usuario'));
+let categoriasUsuario = JSON.parse(localStorage.getItem('categoriasUsuario'));
 
 if (usuario) {
     console.log('Usuario logueado:', usuario);
     var span = document.getElementById('userAlias');
     var pagLog = document.getElementById('linkLogin');
-    pagLog.style.display = 'none'; //Ocultar le iniciar sesión
-    span.style.display = 'inline'; // Mostrar el elemento
-    //poner el display que se vea
+    pagLog.style.display = 'none'; // Ocultar el enlace de iniciar sesión
+    span.style.display = 'inline'; // Mostrar el elemento de bienvenida
+
+    // Mostrar el alias del usuario y el ícono de cierre de sesión
     span.innerHTML = `Hola, ${usuario.Alias} 
             <a href="#" id="logoutButton" title="Cerrar sesión">
                 <i class="fa-solid fa-right-from-bracket" style="color: #ED1C24; margin-left: 10px;"></i>
             </a>`;
+
+    // Mostrar las categorías en el índice
+    console.log('Categorías del usuario logueado:', categoriasUsuario);    
 }
