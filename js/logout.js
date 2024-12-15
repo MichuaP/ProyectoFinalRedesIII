510f2836b837d18cd95e0019f8b0963569b766ae
Mich <paugam21@outlook.com>
2024-12-15 17:37:22 -0600

    Encriptar y bug
    Se encriptan las contraseñas antes de enviarlas al servidor.
    Bug de categorias eliminado

--- js/logout.js
+++ js/logout.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (logoutButton) {
         logoutButton.addEventListener('click', async () => {
             localStorage.removeItem('usuario'); // Eliminar información del usuario
+            localStorage.removeItem('categoriasUsuario'); // Eliminar categorias
             await Swal.fire({
                 title: "Salió de su cuenta",
                 text: "Ha salido correctamente su cuenta",
